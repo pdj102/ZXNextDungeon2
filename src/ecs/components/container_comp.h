c5632f129c54e4d5fa3b69fcf2b1a7d2fdcf0b3d
Paul Johnson <pdj102@yahoo.co.uk>
2025-12-08 21:15:28 +0000

    WIP - refactor components

--- src/ecs/components/container_comp.h
+++ src/ecs/components/container_comp.h
@@ -19,7 +19,14 @@
 /***************************************************
  * public types
  ***************************************************/
+/* Container component data per entity */
+typedef struct {
+    entity_id_t head;   /* Linked list head for entities within container */
+    uint8_t capacity;   /* Maximum number of entities in the container */
+    uint8_t count;      /* Current count of entities in the container */
+} container_comp_t;
 
+typedef container_comp_t container_components_t[MAX_ENTITIES]; /* container component data */
 
 /***************************************************
  * public function prototypes
